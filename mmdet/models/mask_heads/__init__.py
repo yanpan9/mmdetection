f96bab3316894ab75d6116fc6b610ab40acc5083
yanpan <cnyanpan@gmail.com>
2020-06-03 18:42:48 +0800

    四倍的mask head

--- mmdet/models/mask_heads/__init__.py
+++ mmdet/models/mask_heads/__init__.py
@@ -4,8 +4,9 @@ from .grid_head import GridHead
 from .htc_mask_head import HTCMaskHead
 from .maskiou_head import MaskIoUHead
 from .double_fcn_mask_head import DoubleFCNMaskHead
+from .ddouble_fcn_mask_head import DDoubleFCNMaskHead
 
 __all__ = [
     'FCNMaskHead', 'HTCMaskHead', 'FusedSemanticHead', 'GridHead',
-    'MaskIoUHead', "DoubleFCNMaskHead"
+    'MaskIoUHead', "DoubleFCNMaskHead", "DDoubleFCNMaskHead"
 ]
